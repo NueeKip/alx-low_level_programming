621493226f655fd60263d22a57ec467aa65fd38b
NueeKip <emmanuel2kipngeno@gmail.com>
2022-08-16 13:51:54 +0300

    Low-Level Programming ALX Africa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the program
+ * @argc: the legnth of arguments
+ * @argv: the array holding the argument
+ * Return: always zero
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
